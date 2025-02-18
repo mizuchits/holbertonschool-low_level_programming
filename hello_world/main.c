319f32b713397f9c80d5005d9a288cb56720690a
root <marin.tom65@gmail.com>
2025-02-18 08:33:29 +0000

    -E?

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+ main - Entry point
+ 
+ Return: Always 0 (Success)
+
+ int main(void)
 {
     return (0);
 }
