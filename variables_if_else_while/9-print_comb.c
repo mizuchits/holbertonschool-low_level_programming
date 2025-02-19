b4d99d10428ca4e844cd089e04bcf06c2f053c80
root <marin.tom65@gmail.com>
2025-02-19 13:32:45 +0000

    betty fix

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,16 +10,16 @@ int main(void)
   int num;
 
   for (num = 0; num <= 9; num++)
-  {
-    putchar((num % 10) + '0');
-    if (num == 9)
-      continue;
+    {
+      putchar((num % 10) + '0');
+      if (num == 9)
+	continue;
 		
-    putchar(',');
-    putchar(' ');
-   }
+      putchar(',');
+      putchar(' ');
+    }
 
-   putchar('\n');
+  putchar('\n');
 
-   return (0);
+  return (0);
 }
