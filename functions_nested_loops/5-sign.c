3a443628cec030bfc0f57b8dc0e0880dd7390c61
root <marin.tom65@gmail.com>
2025-02-20 13:48:15 +0000

    ez

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@
  */
 int print_sign(int n)
 {
-if (n >= 1)
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-if (n <= -1)
+if (n == 0)
 {
-_putchar('/');
-return (-1);
+_putchar('0');
+return (0);
 }
  else
- _putchar('0');
-return (0);
+ _putchar('/');
+return (-1);
 }
