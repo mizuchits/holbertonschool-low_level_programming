6cd1919214d173518784506c6f98898b578befe9
root <marin.tom65@gmail.com>
2025-02-20 13:51:22 +0000

    betty fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 
 /**
  * print_sign - Checks if a character is lowercase.
- * @c: The character to be checked.
+ * @n: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
