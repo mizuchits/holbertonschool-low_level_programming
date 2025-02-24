db1ec392f06cbb96ac149fd388685005476773e9
root <marin.tom65@gmail.com>
2025-02-24 10:02:48 +0000

    long lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - Prints all single dig
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int count;
+if (n >=1)
+{
+for(count = 0; count <= n; count++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
