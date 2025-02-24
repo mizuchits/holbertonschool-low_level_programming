379bb9d1d01b8925fad2028709059ccf8e1f193e
root <marin.tom65@gmail.com>
2025-02-24 09:28:57 +0000

    bon..

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,17 @@
  */
 void print_most_numbers(void)
 {
-int num;
-for (num = '0'; num <= '9'; num++)
-if ((num == 2) || (num == 4))
+int num = 0;
+for (; num <= 9; num++)
+{
+if (num == 2 || num == 4)
 {
 continue;
 }
 else
 {
-_putchar(num);
-_putchar('\n');
+_putchar(num + '0');
 }
+}
+_putchar('\n');
 }
