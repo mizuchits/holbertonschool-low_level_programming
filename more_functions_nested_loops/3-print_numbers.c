d2ee310a94aa28f86e170f2575a097412daa8027
root <marin.tom65@gmail.com>
2025-02-24 09:03:06 +0000

    betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,8 @@
  */
 void print_numbers(void)
 {
-  int num;;
-
-	for (num = '0'; num <= '9'; num++)
-		_putchar(num);
-
-	_putchar('\n');
+int num;;
+for (num = '0'; num <= '9'; num++)
+_putchar(num);
+_putchar('\n');
 }
