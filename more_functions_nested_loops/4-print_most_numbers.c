f15427b9c8987376063c83b7226cd9c530a1bf26
root <marin.tom65@gmail.com>
2025-02-24 09:17:19 +0000

    some numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+if (num == 2) || (num == 4)
+{
+continue;
+}
+else
+{
+_putchar(num);
+_putchar('\n');
+}
+}
