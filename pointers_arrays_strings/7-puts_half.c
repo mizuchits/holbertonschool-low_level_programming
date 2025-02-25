0ec6cc8c2e5f20129d393094166e43c69a416ef4
root <marin.tom65@gmail.com>
2025-02-25 12:41:14 +0000

    betty

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,32 +2,30 @@
 #include <stdio.h>
 
 /**
- * puts2 - check the code.
+ * puts_half - check the code.
  *
  *@str: charctere
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
-  int i;
-  int count = 0;
-  
-  while (*(str + count) != '\0')
-    {
-      count++;
-    }
-  
-  if (count % 2 == 0)
-    {
-      for (i = count / 2; str[i] != '\0'; i++)
-	_putchar(str[i]);
-    }
-  else
-    {
-      for (i = (count + 1) / 2; str[i] != '\0'; i++)
-	{
-	  _putchar(str[i]);
-	}
-    }
-  _putchar('\n');
+int i;
+int count = 0;
+while (*(str + count) != '\0')
+{
+count++;
+}
+if (count % 2 == 0)
+{
+for (i = count / 2; str[i] != '\0'; i++)
+_putchar(str[i]);
+}
+else
+{
+for (i = (count + 1) / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
 }
