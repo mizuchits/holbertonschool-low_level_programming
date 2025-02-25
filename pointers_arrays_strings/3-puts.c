43d72fce2356baa2f18bcc729f39a1da60f72694
root <marin.tom65@gmail.com>
2025-02-25 09:26:33 +0000

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - check the code.
+ * _puts - check the code.
  *
- *@n: charctere
+ *@str: charctere
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-  int i;
-  for (i = 0;str[i] != '\0'; i++)
-    {
-  _putchar(str[i]);
-    }
-  _putchar('\n');
+int i;
+for (i = 0;str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
