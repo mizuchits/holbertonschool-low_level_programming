f1c136916c7343b53820ca03a4796024b97bd043
root <marin.tom65@gmail.com>
2025-02-25 09:05:31 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code.
+ *
+ *@s: charctere
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int count = 0;
+while (*(s + count) != '\0')
+count++;
+return (count); 
+}
