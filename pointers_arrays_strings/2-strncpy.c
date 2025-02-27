a7da3c34d3325917f177ad5cb0f47546138f25f1
root <marin.tom65@gmail.com.com>
2025-02-27 13:42:48 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -27,9 +27,9 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 for (i = 0; src[i] != '\0'; i++)
 {
-if (i < n)
+if (i > n)
 dest[i] = src[i];
-		i++;
+i++;
 }
 dest[i] = '\0';
 return (dest);
