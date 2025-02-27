81ea4853aced3827cf915c4324550fa6516c6c83
root <marin.tom65@gmail.com.com>
2025-02-27 08:54:06 +0000

    stracat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,8 @@
 
 /**
  * _strlen - check the code.
- *_strcat - ez
  *
  *@s: charctere
- *@dest: ez
- *@src: ez
  * Return: Always 0.
  */
 int _strlen(char *s)
@@ -15,18 +12,24 @@ int _strlen(char *s)
 int count = 0;
 while (*(s + count) != '\0')
 count++;
- return (count);
+return (count);
 }
 
-
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: A pointer to the destination string.
+ * @src: A pointer to the source string to be appended.
+ *
+ * Return: A pointer to the resulting destination string (dest).
+ */
 char *_strcat(char *dest, char *src)
 {
-int i,j;
-j=_strlen(dest);
-for(i=0; src[i] != '\0'; i++)  
+int i, j;
+j = _strlen(dest);
+for (i = 0; src[i] != '\0'; i++)
 {
-dest[i+j]=src[i];
+dest[i + j] = src[i];
 }
-dest[i+j]='\0';
+dest[i + j] = '\0';
 return (dest);
 }
