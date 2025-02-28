0743b4b9e6719881e37a581d28c2e30f64876565
root <marin.tom65@gmail.com.com>
2025-02-28 10:32:33 +0000

    betty

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /**
- * print_array - print
+ * string_toupper - print
  *
- * @a: array to be printed
- * @n: number of elements to print
+ * @*: array to be printed
+ * 
  */
 char *string_toupper(char *str)
 {
-  int i;
-  for(i = 0; str[i] != '\0'; i++)
-    {
-      if(str[i] >= 'a' && str[i] <= 'z')
-        {
-	  str[i] = str[i] - 32;
-        }
-    }
-  return str;
+int i;
+for(i = 0; str[i] != '\0'; i++)
+{
+if(str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+return str;
 }
