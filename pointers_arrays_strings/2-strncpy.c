984b0f74ddf85b6dcd57ad0356d8a6f1f32769de
root <marin.tom65@gmail.com.com>
2025-02-28 08:31:50 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,36 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strlen - check the code.
- *
- *@s: charctere
- * Return: Always 0.
- */
-int _strlen(char *s)
-{
-int count = 0;
-while (*(s + count) != '\0')
-count++;
-return (count);
-}
-
-/**
- * _strncpy - Concatenates two strings.
- * @dest: A pointer to the destination string.
- * @src: A pointer to the source string to be appended.
- * @n: ez
- * Return: A pointer to the resulting destination string (dest).
+ * _strncpy - Copies
+ * @dest: aaaahhh
+ * @src: ez
+ * @
+ * Return: A
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; src[i] != '\0'; i++)
-{
-if (i > n)
-dest[i] = src[i];
-i++;
-}
-dest[i] = '\0';
-return (dest);
+	int index = 0;
+
+	while (src[index] != '\0')
+	{
+	  if (index < n)
+	    dest[index] = src[index];
+	  index++;
+	}
+	dest[index] = '\0';
+	return (dest);
 }
