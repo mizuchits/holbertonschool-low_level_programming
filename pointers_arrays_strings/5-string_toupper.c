baa22e174ad04be5b57cafc176a4377461f81881
root <marin.tom65@gmail.com.com>
2025-02-28 10:42:10 +0000

    betty

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -5,17 +5,17 @@
  * string_toupper - print
  *
  * @*: array to be printed
- * 
+ *
  */
 char *string_toupper(char *str)
 {
 int i;
-for(i = 0; str[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
-if(str[i] >= 'a' && str[i] <= 'z')
+if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - 32;
 }
 }
-return str;
+return (str);
 }
